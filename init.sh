5db0ee306907975e58eabe4c9b79e22259a74af4
Sven Krefeld <info@svenkrefeld.de>
2022-03-30 17:31:15 +0200

    Update the dev setup

--- init.sh
+++ init.sh
@@ -2,12 +2,8 @@
 trap 'echo -e "\e[0;32m" && echo -ne $(date "+%Y-%m-%d %H:%M:%S") && echo " >> Executing: $BASH_COMMAND" && echo -e "\e[0m"' DEBUG
 
 mysql -h127.0.0.1 -uroot -e "CREATE DATABASE application"
-# mysql -h127.0.0.1 -uroot application < application.sql
+mysql -h127.0.0.1 -uroot application < dump.sql
 
-composer install
-
-php bin/console kimai:install -n
-
-php bin/console kimai:user:create sven info@svenkrefeld.de ROLE_SUPER_ADMIN
+composer install --optimize-autoloader -n
 
 trap - DEBUG
