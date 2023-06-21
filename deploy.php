51ed0068c9a52210e46b741d2ffdea81b8ce8745
Sven Krefeld <info@svenkrefeld.de>
2023-06-21 23:01:39 +0200

    Update deployer setup

--- deploy.php
+++ deploy.php
@@ -12,53 +12,53 @@ set('repository', 'git@github.com:svenkrefeld/kimai.git');
 // Allocate tty for git clone. Default value is false.
 set('git_tty', false);
 
-set('bin/php','/usr/local/php81/bin/php');
+set('bin/php','/usr/local/php81/bin/php -d memory_limit=-1');
 set('bin/composer', '/usr/local/php81/bin/php -d memory_limit=-1 ~/.bin/composer');
+
+set('http_user', 'hosting136608');
+set('writable_mode', 'chmod');
+
+set('keep_releases', 3);
 
 // Shared files/dirs between deploys 
 set('shared_files', ['.env']);
 set('shared_dirs', []);
 
 // Writable dirs by web server 
-set('writable_dirs', []);
+set('writable_dirs', ['var']);
 set('allow_anonymous_stats', false);
 
 // Hosts
 host('svenkrefeld.de')
-    ->stage('production')
     ->set('branch', 'main')
-    ->roles('app')
-    ->user('hosting136608')
-    ->port(22)
-    ->forwardAgent(true)
-    ->multiplexing(false)
-    ->set('deploy_path', '~/sites/{{application}}');
+    ->setRemoteUser('hosting136608')
+    ->setPort(22)
+    ->setForwardAgent(true)
+    ->setSshMultiplexing(false)
+    ->setDeployPath('~/sites/{{application}}');
 
 // Tasks
 desc('Deploy your project');
 task('deploy', [
-    'deploy:info',
     'deploy:prepare',
-    'deploy:lock',
-    'deploy:release',
-    'deploy:update_code',
-    'deploy:shared',
-    'deploy:writable',
     'deploy:vendors',
-    'deploy:clear_paths',
-    'deploy:symlink',
-    'deploy:unlock',
-    'cleanup',
-    'success'
+    'deploy:cache:clear',
+    'deploy:publish',
 ]);
 
 desc('Installing kimai');
-task('build', function () {
-    run('cd {{release_path}} && bin/console kimai:install -n');
-    run('cd {{release_path}} && bin/console kimai:reload');
+task('deploy:build', function () {
+    run('cd {{release_path}} && bin/console kimai:update');
 });
 
-after('deploy:vendors', 'build');
+desc('Clears cache');
+task('deploy:cache:clear', function () {
+    if (false !== strpos(get('composer_options', ''), '--no-scripts')) {
+        run('{{bin/console}} cache:clear {{console_options}}');
+    }
+});
+
+after('deploy:vendors', 'deploy:build');
 
 // If deploy fails automatically unlock.
 after('deploy:failed', 'deploy:unlock');
